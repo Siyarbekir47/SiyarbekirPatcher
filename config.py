c4f323290186c732467d10603ef5c1f219db1414
Ramazan <ramazan.du97@gmail.com>
2024-12-25 16:25:06 +0100

    enable scaled contents for image label in main window UI

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import logging
-CURRENT_VERSION = "1.6.0"
+CURRENT_VERSION = "1.6.1"
 VERSION_URL = "https://siyarbekir.de/downloads/patcherversion.json"
 # App-IDs für verschiedene Spiele
 APP_IDS = {
